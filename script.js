e534c876e94933fbf68a1b64af0bfb15b34f43d7
maks-alex <65873569+maks-alex@users.noreply.github.com>
2021-01-03 09:41:27 +0200

    Save before changes

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //prompt('how old are you', '');
+
+function addNote() {
+    
+}
+
+function saveNote() {
+    
+}
+
+function editNote() {
+    
+}
+
+function deleteNote() {
+    
+}
+
+function displayNote() {
+    
+}
+
