122eece4c210747f546cbb08fa839a03030303f7
maks-alex <65873569+maks-alex@users.noreply.github.com>
2021-01-09 23:58:59 +0200

    ok - add note, localStorage, noteList; next - noteDisplay; problem - upload notes for page reload

--- script.js
+++ script.js
@@ -1,52 +1,77 @@
 //prompt('how old are you', '');
 
-function dateTime() {
-    let currentDateTime = new Date();
-    let year = currentDateTime.getFullYear();
-    let month = currentDateTime.getMonth()+1;
-    let day = currentDateTime.getDate();
-    let hour = currentDateTime.getHours();
-    let minuts = currentDateTime.getMinutes();
-    let seconds = currentDateTime.getSeconds()
+function uniqueValues(){
+    return (Date.now() + Math.random()).toString(10)
+}
 
-    return (year+"/"+month+"/"+day+"-"+hour+":"+minuts+":"+seconds)
+function dateTime() {
+     let formatter = new Intl.DateTimeFormat("ru", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    });
+    return (formatter.format(new Date()))
 }
 
 function addNote() {
-    let title = document.getElementById("note-title").value;
-    let note = document.getElementById("note-text").value;
+    const title = document.getElementById("note-title").value;
+    const note = document.getElementById("note-text").value;
 
-    title.value = "введите тему новой заметки";
-    note.value = "введите текст новой заметки";
+    title.value = "";       /*введите тему новой заметки*/
+    note.value = "";        /*введите текст новой заметки*/
 }
 
 function saveNote() {
+                                            /*данные для заметки*/
     let ul = document.getElementById("notes-list");
     let title = document.getElementById("note-title").value;
+    if (title === "") { title = prompt("заполните тему заметки", "Тема") };
+    if (title === "") { alert("заполните тему заметки"); return };
     let note = document.getElementById("note-text").value;
     let date = dateTime();
-    // localStorage.setItem(title, note);
+    let uniqueValue = uniqueValues();
+    let noteID = (title + uniqueValue);
+                                            /*объектJSON для localStorage*/
+    let noteObj = {
+        title: title,
+        note: note,
+        date: date,
+        noteID: noteID
+    }
+    let noteJSON = JSON.stringify(noteObj);
 
+    localStorage.setItem(noteID, noteJSON);
+/*window.localStorage.clear();     window.localStorage.getItem('user');  localStorage.length*/
+/* проверить содержимое localStorage
+for(let i=0; i<localStorage.length; i++) {
+    let key = localStorage.key(i);
+    alert(`${key}: ${localStorage.getItem(key)}`);}
+*/
+
+                                            /*отобразить заметки в перечне заметок*/
     let newNote = document.createElement("li");
     newNote.className = "note-list-elem";
-    newNote.id = note;
+    newNote.id = noteID;
     newNote.innerHTML =
         `
         <div class="notes-title">${title}</div>               
         <div class="notes-text">
             <span class="date">${date}</span>
-            <span class="text">${note}</span>
-        </div>`
-    
-      /*<div class="note-title">Note title</div>
-        <div class="note-text">
-            <span class="date">Сегодня, 17:20</span>
-            <span class="text">text</span>
-        </div>*/
-    ul.prepend(newNote)
 
-
+        </div>
+        <button id="removeBtn-${noteID}">Remove</button>
+        `
+/*<span class="text">${note}</span>*/       /*в template в перечне была часть заметки, оставил темы*/    
+     ul.prepend(newNote)
 }
+
+function displayNote() {
+    
+}
+
 
 function editNote() {
     
@@ -56,17 +81,31 @@ function deleteNote() {
     
 }
 
-function displayNote() {
-    
-}
 
-function testfoo() {
-    alert (1);
-}
-document.getElementById("test-btn").addEventListener("click", testfoo);
+
+
 
 document.getElementById("add-btn").addEventListener("click", addNote);
 document.getElementById("save-btn").addEventListener("click", saveNote);
 document.getElementById("edit-btn").addEventListener("click", editNote);
 
+// function testfoo() {
+//     alert (1);
+// }
+// document.getElementById("test-btn").addEventListener("click", testfoo);
 
+function testTimestamp2() {
+    let date = new Date
+    let unicID = date.getTime()
+    return unicID
+}
+
+function testTimestamp1() {
+    let unicID = (new Date).getTime()
+    return unicID
+}
+
+function testTimestamp() {
+    let unicID = (Date.now() + Math.random()).toString(10);
+    return (A + unicID)
+}
